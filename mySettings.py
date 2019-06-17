4863d9baf4747c1a72d801996af8cb854f219fa6
Harun Reşit Ç <harunresit2085@gmail.com>
2019-06-17 04:08:02 +0300

    Update mySettings.py

--- mySettings.py
+++ mySettings.py
@@ -1,20 +1,20 @@
 #!/usr/bin python
 
-SCRIPT_PATH="/home/pi/Desktop/acilistanSonra/restarted_v2"
+SCRIPT_PATH="/where/is/files"
 
 # E-mail settings
 emailnot=1 # E-mail notification 1 or 0 1=yes, 0=no :)
 # Sender Adress
-senderaddress = "herace@gmail.com"
-senderpassword = "l0e3t0u4r8e5"
+senderaddress = "blabla@gmail.com"
+senderpassword = "mailpassword"
 # Notificaton Receiver Adress
-recaddress = "harunresitc@gmail.com"
+recaddress = "to-who@mail.com"
 # Mail Subject
 mailsubject="Raspberry Warning"
 
 
 #Pushbullet settings
 pushbulletnot=1 # Pushbullet notification 1 or 0 1=yes, 0=no :P
-pbACCESS_TOKEN="o.ggr8aMEH60dGk8Brjr2PSra8Exz3tfMx"
+pbACCESS_TOKEN="PushbulletAccessToken"
 pbMsgTitle="Raspberry Warning"
 
